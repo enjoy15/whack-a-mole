8ed22f4ee0ae26ca56b2afe447d07a2eae4c4981
joy <narumoljoy@gmail.com>
2021-04-22 21:44:59 +0100

    second commit

--- script.js
+++ script.js
@@ -1,24 +1,67 @@
-const boxs = document.querySelectorAll(".grid-item");
-const length = boxs.length;
+//"use strict";
 
-function gridClick(id) {
-    alert(id);
+const coords=document.getElementById("coords")
+const board=document.getElementById("board")
+const hammer=document.getElementById("hammer")
+
+board.addEventListener("mousemove",handleMouseMove)
+board.addEventListener("mousedown",handleMouseDown)
+board.addEventListener("mouseup",handleMouseUp)
+board.addEventListener("keyup",keyUp)
+
+function handleMouseMove(e){
+     coords.innerText= e.clientX + " " + e.clientY
+     hammer.style.top=e.clientY+"px"
+     hammer.style.left=e.clientX +"px"
+}
+function handleMouseDown(e){
+     hammer.classList.add("down")
+}
+function handleMouseUp(e){
+     hammer.classList.remove("down")            
+}
+function keyUp(e){
+     board.innerHTML=e.key
 }
 
-let startButton = document.getElementById('startButton');
+let x = 1; 
 
-var interval = setInterval(() => {
-    //Generate a random number
-    const random = Math.floor(Math.random() * length);
+function gridClick(id) {
+
+    let molePosition = id;
+    alert(molePosition);
+
+}
+
+function mole(){
+
+    var elem = document.getElementById(x);
+    elem.style.backgroundColor = 'white';    
+    x = Math.floor(Math.random()*15)+1;
+    var elem = document.getElementById(x);
+    elem.style.backgroundColor = 'red';
+
+};
+
+
+// let ids = document.getElementById('grid-item');
+// let startButton = document.getElementById('startButton');
+
+// let start = function() {
+//     ids.style.backgroundColor = 'black';
+//   };
+// //   startButton.onclick = start;
+
+//   startButton.addEventListener('click', start);
+
+// startButton.onclick = setInterval(() => {
+//     //Generate a random number
+//     const random = Math.floor(Math.random() * 16);
     
-    //Remove the active class from every ground
-    boxs.forEach((e) => {
-      e.classList.remove("active");
-    });
-    
-    //Add the active class to random ground
-    boxs[random].classList.add("active");
-  }, 700);
+//     const element = document.querySelector('#id');
+//     element.style.color = '#000000'
+
+//   }, 1000);
 
 
 // startButton.onclick = setInterval(function popup(){
